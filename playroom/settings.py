98dc54f3c535b6be90db1d455988bd8a9f4c811a
klamlamwork <klamlamwork@gmail.com>
2026-06-18 12:56:09 -0700

    Update settings.py

--- playroom/settings.py
+++ playroom/settings.py
@@ -56,11 +56,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'playroom.wsgi.application'
 
 # Database
-# This guarantees it connects to Supabase on port 5432
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
