41e5f824eb05d98aabfca59543b431eb1f1b1b64
klamlamwork <klamlamwork@gmail.com>
2026-06-18 13:18:06 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell -c "
+import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(DJANGO_SUPERUSER_USERNAME,DJANGO_SUPERUSER_EMAIL,DJANGO_SUPERUSER_PASSWORD)
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
 "
