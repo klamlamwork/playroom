1a9b2fe0066092dcb638546ccf21a8a3c433201f
klamlamwork <klamlamwork@gmail.com>
2026-06-18 13:04:36 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'changeme123')
+    User.objects.create_superuser(DJANGO_SUPERUSER_USERNAME,DJANGO_SUPERUSER_EMAIL,DJANGO_SUPERUSER_PASSWORD)
 "
