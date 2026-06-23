4c45c54ebd939638d06e8a81d3c04a80edfd6bf2
klamlamwork <klamlamwork@gmail.com>
2026-06-22 17:03:53 -0700

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('chat/', views.chat_view, name='chatbot'),
     #path('ai-plan/', views.ai_plan_recommend, name='ai_plan_recommend'),
+    path('gemini-test/', views.gemini_test, name='gemini_test'),
 ]
