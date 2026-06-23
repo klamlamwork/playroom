1fefbdf4e9b2f0a242a09525e3143fb81f3e4c5e
klamlamwork <klamlamwork@gmail.com>
2026-06-22 17:23:33 -0700

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,10 +2,10 @@
 #chatbot/urls.py
 from django.urls import path
 from . import views
-from . import views_test   # ← Add this line
+#from . import views_test   # ← Add this line
 
 urlpatterns = [
     path('chat/', views.chat_view, name='chatbot'),
-    path('gemini-simple-test/', views_test.gemini_simple_test, name='gemini_simple_test'),
+    #path('gemini-simple-test/', views_test.gemini_simple_test, name='gemini_simple_test'),
     #path('ai-plan/', views.ai_plan_recommend, name='ai_plan_recommend'),
 ]
